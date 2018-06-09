5a1ee04742f2853c965efa8851accc99d4cd7f69
Zhooka <ironjone@gmail.com>
2018-06-09 19:16:54 +0700

    серверулька

--- src/nktl/generator/DwarfMap.java
+++ src/nktl/generator/DwarfMap.java
@@ -3,6 +3,7 @@ package nktl.generator;
 import nktl.math.RangeInt;
 import nktl.math.geom.Vec3i;
 
+import java.util.LinkedList;
 import java.util.Random;
 
 public class DwarfMap {
@@ -13,15 +14,21 @@ public class DwarfMap {
              rangeY = new RangeInt(),
              rangeZ = new RangeInt();
 
+    int dx, dy, dz;
+
     public int layerSize;
 
     DwarfMap(Vec3i dimensions){
+        this.dx = dimensions.x;
+        this.dy = dimensions.y;
+        this.dz = dimensions.z;
 
         rangeX.set(0, dimensions.x-1);
         rangeY.set(0, dimensions.y-1);
         rangeZ.set(0, dimensions.z-1);
 
-        layerSize = rangeX.span() * rangeY.span();
+
+        layerSize = dx * dy;
 
         int map_size = dimensions.x*dimensions.y*dimensions.z;
         map = new DwarfCube[map_size];
@@ -31,19 +38,31 @@ public class DwarfMap {
         return rangeX.has(pos.x) && rangeY.has(pos.y) && rangeZ.has(pos.z);
     }
 
+    // Позиция
+    private int inMapPosition(Vec3i pos){
+        return pos.x + pos.y*dx + pos.z*layerSize;
+    }
+
+    private Vec3i coordinates(int mapPos) {
+        int z = mapPos/layerSize;
+        mapPos -= z;
+        int y = mapPos/dx;
+        int x = mapPos - y;
+        return new Vec3i(x, y, z);
+    }
+
+
+    // Геты
     public DwarfCube get(Vec3i pos){
-        int position = pos.x + pos.y*rangeX.span() + pos.z*layerSize;
-        return map[position];
+        return map[inMapPosition(pos)];
     }
 
     public DwarfCube get(int x, int y, int z){
-        int position = x + y*rangeX.span() + z*layerSize;
-        return map[position];
+        return map[inMapPosition(new Vec3i(x, y, z))];
     }
 
     public void createCubeAt(Vec3i pos){
-        int position = pos.x + pos.y*rangeX.span() + pos.z*layerSize;
-        map[position] = new DwarfCube();
+        map[inMapPosition(pos)] = new DwarfCube();
     }
 
     public Vec3i getRandomPosition(int level, Random random) throws GeneratorException {
@@ -73,4 +92,14 @@ public class DwarfMap {
         return hasBlock;
     }
 
+    public LinkedList<DwarfCube> toCubeList(){
+        LinkedList<DwarfCube> cubeList = new LinkedList<>();
+        for (int i = 0; i < map.length; i++) {
+            if (map[i] != null){
+                map[i].position = coordinates(i);
+                cubeList.add(map[i]);
+            }
+        }
+        return cubeList;
+    }
 }
