f7762ee86feb9c45152a5e16141fd0543fb7f122
Zhooka <ironjone@gmail.com>
2018-06-09 19:25:15 +0700

    серверулька

--- src/nktl/generator/test/GenTest.java
+++ src/nktl/generator/test/GenTest.java
@@ -1,12 +1,13 @@
 package nktl.generator.test;
 
+import nktl.generator.DwarfMap;
+import nktl.generator.Generator;
+import nktl.generator.GeneratorException;
 import nktl.math.RangeInt;
 
 public class GenTest {
-    public static void main(String[]args){
-        RangeInt r = new RangeInt(0, 5);
-
-        System.out.println(RangeInt.from01(0, r));
-        System.out.println(RangeInt.from01(1, r));
+    public static void main(String[]args) throws GeneratorException {
+        Generator generator = new Generator(4, 0.7);
+        DwarfMap dm = generator.generateBooleanMap(100, 100, 3);
     }
 }
