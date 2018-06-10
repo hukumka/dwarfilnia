1365c5643bb67f995bab22c346973d0cf0057f7e
hukumka <hukumka212@gmail.com>
2018-06-10 19:06:16 +0700

    Improved corridors

--- src/nktl/writer/blocks/Corridor.java
+++ src/nktl/writer/blocks/Corridor.java
@@ -1,6 +1,7 @@
 package nktl.writer.blocks;
 
 import nktl.generator.DwarfCube;
+import nktl.math.geom.Direction;
 import nktl.math.geom.Vec3i;
 import nktl.server.MinecraftRMIProcess;
 import nktl.server.commands.Command;
@@ -10,7 +11,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Corridor implements DwarfBlock{
-    private DwarfCube cube=null;
+    private int data=0xf;
+
+    public Corridor setData(int data){
+        this.data = data;
+        return this;
+    }
 
     @Override
     public void placeAt(MinecraftRMIProcess process, Vec3i position) throws IOException{
@@ -65,9 +71,77 @@ public class Corridor implements DwarfBlock{
         );
 
 
+        int[] bits = {
+                DwarfCube.DIRECTION_EAST_BIT,
+                DwarfCube.DIRECTION_SOUTH_BIT,
+                DwarfCube.DIRECTION_WEST_BIT,
+                DwarfCube.DIRECTION_NORTH_BIT
+        };
+        Vec3i center = position.plus(2, 2, 2);
+        for(int i=0; i<4; ++i) {
+            if(is(bits[i])){
+                Fill f = new Fill(
+                        position.plus(4, 1, 1),
+                        position.plus(4, 3, 3),
+                        "minecraft:air"
+                );
+                Fill fix_stair = null;
+                Fill fix_stair2 = null;
+                Fill fix_stair3 = null;
+                Fill fix_stair4 = null;
+                if(!is(bits[(i+3)%4])){ // test if not clockwise corridor
+                    fix_stair = new Fill(
+                            position.plus(4, 1, 0),
+                            position.plus(4, 1, 0),
+                            "minecraft:stone_brick_stairs"
+                    ).dataValue(3);
+                    fix_stair2 = new Fill(
+                            position.plus(4, 3, 0),
+                            position.plus(4, 3, 0),
+                            "minecraft:stone_brick_stairs"
+                    ).dataValue(7);
+                }
+                if(is(bits[(i+1)%4])){ // test if not clockwise corridor
+                    fix_stair3 = new Fill(
+                            position.plus(0, 1, 0),
+                            position.plus(0, 1, 0),
+                            "minecraft:stone_brick_stairs"
+                    ).dataValue(1);
+                    fix_stair4 = new Fill(
+                            position.plus(0, 3, 0),
+                            position.plus(0, 3, 0),
+                            "minecraft:stone_brick_stairs"
+                    ).dataValue(5);
+                }
+                for(int j=0; j<i; ++j){
+                    f.rotate90(center.x, center.z);
+                    if(fix_stair != null){
+                        fix_stair.rotate90(center.x, center.z);
+                        fix_stair2.rotate90(center.x, center.z);
+                    }
+                    if(fix_stair3 != null){
+                        fix_stair3.rotate90(center.x, center.z);
+                        fix_stair4.rotate90(center.x, center.z);
+                    }
+                }
+                commands.add(f);
+                if(fix_stair != null){
+                    commands.add(fix_stair);
+                    commands.add(fix_stair2);
+                }
+                if(fix_stair3 != null){
+                    commands.add(fix_stair3);
+                    commands.add(fix_stair4);
+                }
+            }
+        }
 
         for(Command c: commands){
             process.write(c.toCommandString());
         }
+    }
+
+    private boolean is(int bit){
+        return (data & bit) > 0;
     }
 }
--- src/nktl/writer/tests/TestBlocks.java
+++ src/nktl/writer/tests/TestBlocks.java
@@ -1,5 +1,6 @@
 package nktl.writer.tests;
 
+import nktl.generator.DwarfCube;
 import nktl.math.geom.Vec3i;
 import nktl.server.MinecraftRMIProcess;
 import nktl.writer.RMIClient;
@@ -12,7 +13,24 @@ public class TestBlocks {
             MinecraftRMIProcess process = new RMIClient().getProcess();
 
             new Corridor()
+                    .setData(DwarfCube.DIRECTION_EAST_BIT | DwarfCube.DIRECTION_NORTH_BIT)
+                    .placeAt(process, new Vec3i(0, 110, 0));
+            new Corridor()
+                    .setData(DwarfCube.DIRECTION_WEST_BIT | DwarfCube.DIRECTION_NORTH_BIT)
+                    .placeAt(process, new Vec3i(5, 110, 0));
+            new Corridor()
+                    .setData(DwarfCube.DIRECTION_WEST_BIT | DwarfCube.DIRECTION_SOUTH_BIT)
+                    .placeAt(process, new Vec3i(5, 110, 5));
+            new Corridor()
+                    .setData(DwarfCube.DIRECTION_EAST_BIT | DwarfCube.DIRECTION_SOUTH_BIT)
+                    .placeAt(process, new Vec3i(0, 110, 5));
+
+            new Corridor()
+                    .setData(0x3)
                     .placeAt(process, new Vec3i(0, 100, 0));
+            new Corridor()
+                    .setData(0x1)
+                    .placeAt(process, new Vec3i(0, 105, 0));
 
         }catch(Exception e){
             e.printStackTrace();
